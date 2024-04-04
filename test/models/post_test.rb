7ead64dba600c46956aa67dba71f737dc3516b3d
Vinicius Tamanini <vinitamanini@hotmail.com>
2024-04-04 00:33:01 -0300

    add tests for post

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? returns true for a draft post" do
+    assert posts(:draft).draft?
+  end
+
+  test "draft? returns false for a published post" do
+    refute posts(:published).draft?
+  end
+
+  test "draft? returns false for a scheduled post" do
+    refute posts(:scheduled).draft?
+  end
+
+  test "published? returns true for a published post" do
+    assert posts(:published).published?
+  end
+
+  test "published? returns false for a draft post" do
+    refute posts(:draft).published?
+  end
+
+  test "published? returns false for a scheduled post" do
+    refute posts(:scheduled).published?
+  end
+
+  test "scheduled? returns true for a scheduled post" do
+    assert posts(:scheduled).scheduled?
+  end
+
+  test "scheduled? returns false for a draft post" do
+    refute posts(:draft).scheduled?
+  end
+
+  test "scheduled? returns false for a published post" do
+    refute posts(:published).scheduled?
+  end
 end
