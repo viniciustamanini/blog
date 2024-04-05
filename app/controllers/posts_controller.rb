2d55c34196382503914e8c14e53043adfb4bc930
Vinicius Tamanini <vinitamanini@hotmail.com>
2024-04-05 00:27:54 -0300

    change body to content on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,7 +41,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body, :published_at)
+    params.require(:post).permit(:title, :content, :published_at)
   end
 
   def set_blog_post
