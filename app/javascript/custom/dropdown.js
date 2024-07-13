52ba69ac8db068ab204158ebb2c591af54babcf9
Vinicius Tamanini <vinitamanini@hotmail.com>
2024-07-13 13:34:07 -0300

    start new dropdown functions

---
+++ app/javascript/custom/dropdown.js
@@ -0,0 +1,20 @@
+console.log('test');
+
+function toggleDropdown() {
+  console.log('toggle');
+  document.getElementById('dropdown-button')
+}
+
+window.onclick = function(event) {
+  console.log('click');
+  if (event.target.matches('.dropbtn'))
+    return;
+
+  var dropdownContent = document.getElementsByClassName('dropdown-menu');
+  for (var i = 0; i < dropdownContent.length; i++) {
+    var openDropdown = dropdownContent[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
+}
